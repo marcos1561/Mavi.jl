5e9767ef0b893741a23c216e49be97b707636522
Vini-BM <vinicius.bayne@ufrgs.br>
2024-10-01 23:35:10 -0300

    RTP implementation

---
+++ examples/rtp.jl
@@ -0,0 +1,53 @@
+"""
+Simple exemple using Run-and-Tumble model
+"""
+
+using Mavi
+using Mavi.InitStates
+using Mavi.Configs
+using Mavi.Visualization
+
+function main()
+    num_particles = 5
+
+    num_p_x = trunc(Int, sqrt(num_particles))
+    num_p_y = trunc(Int, sqrt(num_particles))
+
+    dynamic_cfg = RunTumbleCfg(
+        vo=1,
+        sigma=1,
+        epsilon=1,
+        tumble_rate=5,
+    )
+
+    pos, geometry_cfg = rectangular_grid(
+        num_p_x,
+        num_p_y,
+        1, # offset
+        particle_radius(dynamic_cfg), # radius
+    )
+
+    space_cfg = SpaceCfg(
+        wall_type=PeriodicWalls(),
+        geometry_cfg=geometry_cfg,
+    )
+
+    system = System(
+        state=SelfPropelledState(
+            pos=pos,
+            pol_angle=rand(Float64, size(pos)[2])*2*π,
+        ),
+        space_cfg=space_cfg,
+        dynamic_cfg=dynamic_cfg,
+        int_cfg=ChunksIntCfg(
+            dt=0.01,
+            chunks_cfg=ChunksCfg(
+                num_cols=num_p_x-1, 
+                num_rows=num_p_y-1,
+            ),
+        ),
+    )
+
+    animate(system, Mavi.Integration.rtp_step!, AnimationCfg(num_steps_per_frame=10))
+end
+main()
