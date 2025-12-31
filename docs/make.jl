083218d61440d9c1a4fd0b909cfc2f6e561a38f9
Marcos Pasa <marcospasa130@gmail.com>
2025-12-31 06:57:12 -0300

    docs: new posts folder name

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,8 @@ makedocs(
         "index.md",
         "manual.md",
         "Blog" => [
-            "_posts/2025-11-11_rings.md",
-            # "_posts/2025-10-05-variable_particle_number.md",
-            "_posts/2025-09-14-space_system.md",
+            "posts/2025-11-11_rings.md",
+            "posts/2025-09-14-space_system.md",
         ]
     ]
     
